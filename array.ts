14821012341c111d20bbd05b6fbba4e83f91a67d
uzairzia <uzair9900@hotmail.com>
2024-03-01 07:21:57 +0500

    first commit

---
+++ array.ts
@@ -0,0 +1,11 @@
+let items: string[] = ['K2', 'Pakistan', 'Lahore','English'];
+
+console.log(items);
+
+let item2: string[]=[];
+
+item2.push('Mount Everest');
+item2.push('India');
+item2.push('Karachi');
+
+console.log(item2);
